0852416286cf06fc7b66290475e4ccf5e3ec7c34
DinaTaklit <ez_taklit@esi.dz>
2020-04-08 13:15:20 +0100

    update: add import statement

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-
+import os
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
